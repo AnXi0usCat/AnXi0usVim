58e998d03df2aaf61edf379e5f32e6de31db3a68
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-03-05 19:15:46 +0000

    add neotree to packer config

--- lua/anxiouscat/packer.lua
+++ lua/anxiouscat/packer.lua
@@ -59,4 +59,14 @@ return require('packer').startup(function(use)
             {'rafamadriz/friendly-snippets'}, -- Optional
         }
     }
+
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = { 
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
+    }
 end)
