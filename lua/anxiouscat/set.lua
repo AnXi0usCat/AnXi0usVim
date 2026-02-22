3dd8cf8217fe9d5ba17b3cf722807985d8e3e437
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2026-02-22 15:46:55 +0000

    disable unused providers

--- lua/anxiouscat/set.lua
+++ lua/anxiouscat/set.lua
@@ -2,6 +2,11 @@
 vim.o.number = true                   -- show line numbers
 vim.o.relativenumber = true           -- number relative to current line
 
+-- Disable unused providers to avoid startup checks.
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- 4 spoace indent
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
@@ -13,7 +18,5 @@ vim.o.smartindent = true
 vim.o.termguicolors = true
 
 vim.o.updatetime = 50
-
-vim.g.mapleader = " "
 
 vim.opt.clipboard = "unnamedplus"
